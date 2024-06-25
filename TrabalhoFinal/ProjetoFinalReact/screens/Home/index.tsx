b3989b290c387a977498b873558792957b730d47
Felipe Mauriz <felipeamorimferreira@gmail.com>
2024-06-24 21:37:26 -0300

    final Home

--- TrabalhoFinal/ProjetoFinalReact/screens/Home/index.tsx
+++ TrabalhoFinal/ProjetoFinalReact/screens/Home/index.tsx
@@ -12,11 +12,20 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import styles from "./styles";
+
 // import Card from "../Card";
 
 const renderListaLateralItem = ({ item }) => (
-  <TouchableOpacity style={styles.listaLateralItem}>
-    <Image style={styles.listaLateralPoster} source={{uri:item.Poster}} />
+  <TouchableOpacity
+    onPress={() => alert(`testando ${item.Title}`)}
+    style={styles.listaLateralItem}
+  >
+    <Image
+      resizeMode="stretch"
+      style={styles.listaLateralPoster}
+      source={{ uri: item.Poster }}
+    />
     <Text style={styles.listaLateralTitle}>{item.Title}</Text>
   </TouchableOpacity>
 );
@@ -26,24 +35,26 @@ export default function Home() {
     {
       imdbID: "tt0120737",
       Title: "Cruella",
-      Poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr2Fo9Nb7bLdyILFYbMZKtEnu-659zCT9DYw&usqp=CAU",
+      Poster:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr2Fo9Nb7bLdyILFYbMZKtEnu-659zCT9DYw&usqp=CAU",
     },
     {
       imdbID: "tt0167261",
       Title: "Raya",
-      Poster: "https://lumiere-a.akamaihd.net/v1/images/1920x1080_logo_355fcbfc.png",
+      Poster:
+        "https://lumiere-a.akamaihd.net/v1/images/1920x1080_logo_355fcbfc.png",
     },
     {
       imdbID: "tt0167262",
       Title: "Luca",
-      Poster: "https://img.odcdn.com.br/wp-content/uploads/2021/06/original_1622571176_Luca_Poster.jpg",
+      Poster:
+        "https://img.odcdn.com.br/wp-content/uploads/2021/06/original_1622571176_Luca_Poster.jpg",
     },
   ]);
   const [busca, setBusca] = useState("");
-  const [filmesInicio, setFilmesInicio] = useState(filmes)
-  const [filmesPesquisados, setFilmesPesquisados] = useState([])
+  const [filmesInicio, setFilmesInicio] = useState(filmes);
+  const [filmesPesquisados, setFilmesPesquisados] = useState([]);
   const obterFilmes = async () => {
-    
     const filmesFiltrados = filmes.filter((filme) =>
       filme.Title.toLowerCase().includes(busca.toLowerCase())
     );
@@ -61,8 +72,8 @@ export default function Home() {
         Poster: res.data.Poster,
       };
 
-      setFilmes([filmeEncontrado])
-      setFilmesPesquisados([...filmesPesquisados, filmeEncontrado])
+      setFilmes([filmeEncontrado]);
+      setFilmesPesquisados([...filmesPesquisados, filmeEncontrado]);
     } catch (error) {
       console.error("Erro ao buscar filmes:", error);
     }
@@ -94,84 +105,30 @@ export default function Home() {
       <ScrollView>
         <View style={styles.encarte}>
           <Text style={styles.sectionTitle}>Filme em Destaque</Text>
-          <Image
-            style={styles.banner}
-            source={require("../../assets/dug.jpeg")}
+          <FlatList
+            horizontal
+            data={[
+              { key: "1", image: require("../../assets/dug.jpeg") },
+              { key: "2", image: require("../../assets/ron.jpeg") },
+              { key: "3", image: require("../../assets/red.jpeg") },
+              { key: "4", image: require("../../assets/raya.jpeg") },
+            ]}
+            renderItem={({ item }) => (
+              <Image style={styles.banner} source={item.image} />
+            )}
+            keyExtractor={(item) => item.key}
           />
         </View>
+
         <View style={styles.listaLateral}>
           <Text style={styles.sectionTitle}>Outros Filmes</Text>
           <FlatList
             data={filmes}
             renderItem={renderListaLateralItem}
             keyExtractor={(item) => item.imdbID}
-            
           />
         </View>
       </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    paddingHorizontal: 20,
-    paddingTop: 20,
-  },
-  header: {
-    width: "100%",
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  logo: {
-    width: 150,
-    height: 50,
-    resizeMode: "contain",
-  },
-  searchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 5,
-    color: "#fff",
-    marginRight: 10,
-  },
-  encarte: {
-    marginBottom: 20,
-  },
-  banner: {
-    width: "100%",
-    height: 200,
-    borderRadius: 10,
-  },
-  sectionTitle: {
-    color: "#fff",
-    fontSize: 24,
-    marginBottom: 10,
-  },
-  listaLateral: {
-    marginBottom: 20,
-    height:"90%",
-    width: "90%",
-  },
-  listaLateralItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  listaLateralPoster: {
-    width: "100%",
-    height: 200,
-    borderRadius: 5,
-  },
-});
---
+++ TrabalhoFinal/ProjetoFinalReact/screens/Home/styles.tsx
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  header: {
+    width: "100%",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  logo: {
+    width: 150,
+    height: 50,
+    resizeMode: "contain",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 5,
+    color: "#fff",
+    marginRight: 10,
+  },
+  encarte: {
+    marginBottom: 20,
+  },
+  banner: {
+    width: 330,
+    height: 200,
+    borderRadius: 10,
+    margin: 5,
+  },
+  sectionTitle: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  listaLateral: {
+    marginBottom: 20,
+    height: "90%",
+    width: "100%",
+  },
+  listaLateralItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  listaLateralPoster: {
+    width: "100%",
+    height: 230,
+    borderRadius: 5,
+  },
+});
+export default styles;
